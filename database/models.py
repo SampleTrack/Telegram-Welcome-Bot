f7acc8745fbf9e19598e87982bf69de375db2e2e
SampleTrack <FirstSample@proton.me>
2026-05-20 19:46:22 +0530

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,11 @@
+# Default group document structure in MongoDB
+
+DEFAULT_GROUP = {
+    "chat_id": None,
+    "welcome_message": None,
+    "rules": None,
+    "welcome_enabled": True,
+    "rules_button": True,
+    "clean_joins": True,
+    "ban_bots": True,
+}
